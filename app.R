735c2c5e4f59ac0a2eebaeba3f457170221294bb
afgerst <149267506+afgerst@users.noreply.github.com>
2023-11-29 09:38:39 -0600

    Add files via upload
    Reuploading with minor changes just in case I mess it up beyond repair trying to make a dashboard :)

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 
 # For creating the interactive app
 library(shiny)
+library(shinydashboard)
 
 # For the world maps
 library(maps) 
@@ -12,8 +13,10 @@ library(leaflet)
 library(dplyr)
 
 
-
+# Reading in data file
 TRI2022 <- read.csv("2022_us.csv")
+
+# Filtering down to 25 variables instead of 119
 TRI2022 <- TRI2022 %>%
   select(YEAR, FACILITY.NAME, CITY, ST, ZIP, LATITUDE, LONGITUDE, PARENT.CO.NAME, INDUSTRY.SECTOR, CHEMICAL, CLEAN.AIR.ACT.CHEMICAL,
          CLASSIFICATION, METAL, CARCINOGEN, PBT, PFAS, UNIT.OF.MEASURE, FUGITIVE.AIR, STACK.AIR, WATER, UNDERGROUND, LANDFILLS,
@@ -23,14 +26,13 @@ ui <- fluidPage(
   
   titlePanel("TRI Zip input test"),
   
-  sidebarLayout(  # Corrected the typo here, it should be sidebarLayout, not sidebarlayout
+  sidebarLayout(  
     sidebarPanel(
       textInput("zip_input", "Enter Zip Code:", ""),
-      actionButton("submit_btn", "Submit")
+      actionButton("submit_btn", "Submit") # So far this is useless, the map changes as soon as a zip is entered
     ),
     mainPanel(
-      tags$style("#mymap {height: 600px;}"),
-      leafletOutput("mymap")  # Corrected the output ID to "mymap"
+      leafletOutput("mymap", height = "80vh")  
     )
   )
 )
@@ -38,22 +40,27 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   output$mymap <- renderLeaflet({
-    zip_code <- as.numeric(input$zip_input)
+    zip_code <- as.numeric(input$zip_input) # make sure widget input is numeric instead of character string
     
-    data_filtered <- TRI2022 %>%
+    # filter data so only points from selected zip are used
+    data_filtered <- TRI2022 %>% 
       filter(ZIP == zip_code)
     
+    # if there are no data points for an entered zip code, show an error
     if (nrow(data_filtered) == 0) {
       leaflet() %>%
         addTiles() %>%
         addMarkers(lat = 0, lng = 0) %>%
         addPopups(lat = 0, lng = 0, content = "There are no release sites in this zip code. Please try another.")
     } 
+    
+    # when a valid zip is entered
     else {
       leaflet(data_filtered) %>%
         addTiles() %>%
         addMarkers(lat = ~LATITUDE, 
                    lng = ~LONGITUDE,
+                   # when a user clicks on a maker info about the facility, industry, and chemical is listed
                    popup = ~paste("Facility:", FACILITY.NAME, "<br>",
                                   "Industry:", INDUSTRY.SECTOR, "<br>",
                                   "Chemical:", CHEMICAL))
